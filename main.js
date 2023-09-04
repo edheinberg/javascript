2154ea0f4b2b77f238be05a097fa6b283ec465b9
edheinberg <edheinberg@gmail.com>
2023-09-04 11:35:06 -0300

    Juego

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
-// Ejemplo desafio entregable
+// PRIMER ENTREGABLE
 
-alert('¡Bienvenido a TocandoLaGuitarra! \n\n El sitio de música más iteractivo que hay');
+// alert('¡Bienvenido a TocandoLaGuitarra! \n\n El sitio de música más iteractivo que hay');
 
-let nombre = prompt('¿Cómo es tu nombre?');
-let apellido = prompt('¿Cómo es tu apellido?');
-let edad = parseInt(prompt('Y ¿Cuál es tu edad?'));
+// let nombre = prompt('¿Cómo es tu nombre?');
+// let apellido = prompt('¿Cómo es tu apellido?');
+// let edad = parseInt(prompt('Y ¿Cuál es tu edad?'));
 
-const ESPACIO = ' ';
+// const ESPACIO = ' ';
 
-if (nombre == '' || apellido == '' || edad == 0) {
-    alert('Los datos ingresados son incorrectos, intenta nuevamente')
-} else {
-    alert('Gracias por visitarnos ' + nombre + ESPACIO + apellido);
+// if (nombre == '' || apellido == '' || edad == 0) {
+//     alert('Los datos ingresados son incorrectos, intenta nuevamente')
+// } else {
+//     alert('Gracias por visitarnos ' + nombre + ESPACIO + apellido);
+// }
+
+alert('Te invitamos a responder unas preguntas para saber cuanto sabes de música')
+alert('Por cada respuesta correcta obtenés 3 puntos de lo contrario no sumarás ninguno')
+alert('Si lográs 9 puntos obtené un IMPORTANTE DESCUENTO en nuestro catálogo!!')
+
+let puntuacion = 0;
+
+for (let i = 1; i <= 3; i += 3) {
+    const respuesta1 = parseInt(prompt('¿Cuánto tiempo vale la nota NEGRA?'));
+
+    if (respuesta1 === 1) {
+        alert("¡Correcto! Ganaste 3 puntos.");
+        puntuacion += 3;
+    } else {
+        alert("Incorrecto. El valor de la nota es 1");
+        puntuacion += 0;
+    }
+
+    const respuesta2 = parseInt(prompt('¿Cuánto tiempo vale la nota REDONDA?'));
+
+    if (respuesta2 === 4) {
+        alert("¡Correcto! Ganaste 3 puntos.");
+        puntuacion += 3;
+    } else {
+        alert("Incorrecto. El valor de la nota es 4");
+        puntuacion += 0;
+    }
+
+    const respuesta3 = parseInt(prompt('¿Cuánto tiempo vale la nota BLANCA?: '));
+
+    if (respuesta3 === 2) {
+        alert("¡Correcto! Ganaste 3 puntos.");
+        puntuacion += 3;
+    } else {
+        alert("Incorrecto. El valor de la nota es 2");
+        puntuacion += 0;
+    }
 }
+
+if (puntuacion < 12) {
+    alert("Fin del juego. Tu puntuación final es: " + puntuacion);
+} else {
+    alert("¡¡FELICITACIONES!!. Tu puntuación final es: " + puntuacion);
+}
+
 
 
 // const comprarProductos = () => {
